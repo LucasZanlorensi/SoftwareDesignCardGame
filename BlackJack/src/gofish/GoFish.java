6405069ca16cce2076f2da3525728fe27601cdb2
schunicd <derek_schunicke@hotmail.com>
2019-02-26 16:02:53 -0500

    Main class additions

--- BlackJack/src/gofish/GoFish.java
+++ BlackJack/src/gofish/GoFish.java
@@ -4,14 +4,56 @@
  * and open the template in the editor.
  */
 package gofish;
+import java.util.Scanner;
 
 /**
  *
  * @author lucas
  */
-public class GoFish {
+public class GoFish 
+{
     
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        
+        Scanner input = new Scanner(System.in);
+        
+        String playAgain;
+        int players;
+        
+        
+        do 
+        {
+        
+            do{
+                
+                System.out.println("Do you want to play Go Fish? (Y/N)");
+        
+                playAgain = input.next().toUpperCase();
+                
+                if(!playAgain.equals("Y") && !playAgain.equals("N"))
+                    System.out.println("Please enter either 'Y' for yes or 'N' for no!");
+            
+            }while(!playAgain.equals("Y") && !playAgain.equals("N"));
+            
+            if(playAgain.equals("Y"))
+            {
+            
+                do{
+                    
+                    System.out.println("How many players would you like to play with? (min 2, max 4):");
+
+                    players = input.nextInt();
+                    
+                    if(players < 2 || players > 4)
+                
+                }while(players < 2 || players > 4);
+                
+            }
+                
+        
+        }while(playAgain.equals("Y") && !playAgain.equals("N"));
         
     }
+    
 }
