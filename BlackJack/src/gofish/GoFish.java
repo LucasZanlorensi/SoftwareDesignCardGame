6322f44df101ac513ef2572c30988312828ad49e
schunicd <derek_schunicke@hotmail.com>
2019-04-07 16:14:25 -0400

    Generate deck in GoFish class, made changes to GoFishCard to reflect these changes.

--- BlackJack/src/gofish/GoFish.java
+++ BlackJack/src/gofish/GoFish.java
@@ -10,8 +10,22 @@ import java.util.ArrayList;
  * @author lucas
  */
 public class GoFish extends Game{
-
+    
     ArrayList<GoFishPlayer> players;
+    private ArrayList <GoFishCard> cards;
+    
+    public void generateHand()
+        {
+                int countCards = 0;
+		for(GoFishCard.suit s: GoFishCard.suit.values())
+                {
+                    for(GoFishCard.value v: GoFishCard.value.values())
+                    {
+                        cards.set(countCards, new GoFishCard(s,v));
+                        countCards++;
+                    }
+                }
+        }
     
     public GoFish(String givenName) {
         super(givenName);
