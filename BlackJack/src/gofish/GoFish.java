6f36e6c764dbc2e9a8ff039796289b12fd64f06d
schunicd <derek_schunicke@hotmail.com>
2019-04-07 20:51:29 -0400

    compare users guess to valid card values and determine if valid guess

--- BlackJack/src/gofish/GoFish.java
+++ BlackJack/src/gofish/GoFish.java
@@ -60,7 +60,6 @@ public class GoFish extends Game{
                     
                     else
                         validGuess = false;
-                        
                 
                 }
                 
